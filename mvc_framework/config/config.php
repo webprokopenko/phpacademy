a8f8bfb94c75a976f5b054c388e6d7782c88b5c7
Oleg <oleg.shumar@gmail.com>
2016-07-25 17:59:18 +0300

    MVC - Lesson 15

--- mvc_framework/config/config.php
+++ mvc_framework/config/config.php
@@ -19,3 +19,5 @@ Config::set('db.host', 'localhost');
 Config::set('db.user', 'root');
 Config::set('db.password', '');
 Config::set('db.db_name', 'mvc');
+
+Config::set('salt', 'jd7sj3sdkd964he7e');
