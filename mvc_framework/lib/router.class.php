bff81b5eaf5fa0d6c007481d382ee90042280977
Oleg <oleg.shumar@gmail.com>
2016-07-25 17:56:23 +0300

    MVC - Lesson 4

---
+++ mvc_framework/lib/router.class.php
@@ -0,0 +1,49 @@
+<?php
+
+class Router{
+
+    protected $uri;
+
+    protected $controller;
+
+    protected $action;
+
+    protected $params;
+
+    /**
+     * @return mixed
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    public function __construct($uri){
+        print_r('Ok! Router was called with uri: '.$uri);
+    }
+
+}
